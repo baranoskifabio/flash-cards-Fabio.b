1f587d7489017722fe0d870151b7b1238a7d61d6
baranoskifabio <145785098+baranoskifabio@users.noreply.github.com>
2024-10-25 08:39:48 -0300

    25\10\2024

---
+++ assets/pergunta.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'futebol',
+    'Quem é o rei do futebol?',
+    'É o Pelé'
+
+)
+
+criaCartao(
+    'basquete',
+    'Quem é o rei do basquete?',
+    'É o Michael Jordan'
+
+)
+
+criaCartao(
+    'corpo humano',
+    'Quantos ossos temos em nosso corpo?',
+    '206'
+
+)
+
+
+criaCartao(
+    'tsunami',
+    'Oque é um Tsunami?',
+    'É um maremoto'
+
+)
